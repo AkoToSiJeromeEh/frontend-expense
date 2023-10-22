02a76c9dbaa4e9d442f37488a6fe801a89e19e12
Jeje <114987334+AkoToSiJeromeEh@users.noreply.github.com>
2023-10-22 22:41:47 +0800

    Update index.js

--- src/service/index.js
+++ src/service/index.js
@@ -1,15 +1,15 @@
 import Axios from "axios";
 const fetchExpenseList = () => {
-    return Axios.get("https://expensetracker-api.onrender/expenses");
+    return Axios.get("https://expensetracker-api-yy05.onrender.com/api/expenses/expenses");
 };
 
 const fetchReminderList = () => {
-    return Axios.get("https://expensetracker-api.onrender/api/reminders");
+    return Axios.get("https://expensetracker-api-yy05.onrender.com/api/reminders");
 };
 
 const fetchIncome = () => {
 
-     return Axios.get("https://expensetracker-api.onrender/api/incomes")
+     return Axios.get("https://expensetracker-api-yy05.onrender.com/api/incomes")
 }
 
 
