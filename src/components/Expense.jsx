c29eb7aa26641356b3f7e62e5e0faca9b885e1aa
Jeje <114987334+AkoToSiJeromeEh@users.noreply.github.com>
2023-10-24 12:31:08 +0800

    Update Expense.jsx

--- src/components/Expense.jsx
+++ src/components/Expense.jsx
@@ -6,6 +6,7 @@ import { BiSolidCameraMovie } from "react-icons/bi";
 import { HiMiniHomeModern } from "react-icons/hi2";
 import { FaMoneyBill } from "react-icons/fa";
 import {RiDeleteBin6Fill} from 'react-icons/ri'
+import cardImage from "../images/Rectangle 2.png";
 
  const Expense = (props) => {
 
@@ -28,7 +29,7 @@ import {RiDeleteBin6Fill} from 'react-icons/ri'
             ) : category == " Entertainment" ? (
               <BiSolidCameraMovie className="w-10 text-2xl" />
             ) : (
-              ""
+              <img className="w-10" src={cardImage} />
             )}
 	  </div>
 	  <div className="inline-block align-middle ms-2">
