e84e85f255c0d717ce81b14a023b9d4d008fb89d
Jeje <114987334+AkoToSiJeromeEh@users.noreply.github.com>
2023-12-16 16:10:01 +0800

    Update index.js

--- src/service/index.js
+++ src/service/index.js
@@ -1,19 +1,19 @@
 import axios from "axios";
 const token = localStorage.getItem("accessToken");
 const fetchExpenseList = () => {
-  return axios.get("https://expensetracker-api-yy05.onrender/api/expenses/expenses", {
+  return axios.get("https://expensetracker-api-yy05.onrender.com/api/expenses/expenses", {
     headers: { Authorization: `Bearer ${token ? token : null}` }
   });
 };
 
 const fetchReminderList = () => {
-  return axios.get("https://expensetracker-api-yy05.onrender/api/reminders", {
+  return axios.get("https://expensetracker-api-yy05.onrender.com/api/reminders", {
     headers: { Authorization: `Bearer ${token ? token : null}` }
   });
 };
 
 const fetchIncome = () => {
-  return axios.get("https://expensetracker-api-yy05.onrender/api/incomes", {
+  return axios.get("https://expensetracker-api-yy05.onrender.com/api/incomes", {
     headers: { Authorization: `Bearer ${token ? token : null}` }
   });
 };
